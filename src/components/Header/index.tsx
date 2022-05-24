4285bb0164e37fff691592a042605032e9580a07
Rigote <matheus.rigote@samuraiexperts.com.br>
2022-05-24 01:01:05 -0300

    [Create] - Start Project

---
+++ src/components/Header/index.tsx
@@ -0,0 +1,13 @@
+import Image from "next/image"
+import { ChevronDownIcon, HomeIcon, MenuIcon, SearchIcon } from '@heroicons/react/solid'
+import { BellIcon, ChatIcon, GlobeIcon, PlusIcon, SparklesIcon, SpeakerphoneIcon, VideoCameraIcon } from '@heroicons/react/outline'
+
+const Header = () => {
+  return (
+    <div className="sticky top-0 z-50 flex bg-white px-4 py-2 shadow-sm">
+
+    </div>
+  )
+}
+
+export default Header
