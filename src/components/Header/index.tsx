55148576e2ce87661e4bde7f5996b08e53fa87de
Rigote <matheus.rigote@samuraiexperts.com.br>
2022-06-22 00:18:15 -0300

    [Feature] - Implementando NextAuth

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,8 +1,10 @@
 import Image from "next/image"
 import { ChevronDownIcon, HomeIcon, MenuIcon, SearchIcon } from '@heroicons/react/solid'
 import { BellIcon, ChatIcon, GlobeIcon, PlusIcon, SparklesIcon, SpeakerphoneIcon, VideoCameraIcon } from '@heroicons/react/outline'
+import { signIn, signOut, useSession } from "next-auth/react"
 
 const Header = () => {
+  const {data: session} = useSession()
   return (
     <div className="sticky top-0 z-50 flex bg-white px-4 py-2 shadow-sm">
       <div className="relative h-10 w-20 flex-shrink-0 cursor-pointer">
@@ -40,12 +42,25 @@ const Header = () => {
       </div>
 
       {/** Sign in / Sign out button */}
-      <div className="hidden cursor-pointer items-center space-x-2 border border-gray-100 p-2 lg:flex">
-        <div className="relative h-5 w-5 flex-shrink-0">
-          <Image src="https://links.papareact.com/23l" layout="fill" objectFit="contain" />
+      {session ? (
+        <div onClick={() => signOut()} className="hidden cursor-pointer items-center space-x-2 border border-gray-100 p-2 lg:flex">
+          <div className="relative h-5 w-5 flex-shrink-0">
+            <Image src="https://links.papareact.com/23l" layout="fill" objectFit="contain" />
+          </div>
+          <div className="flex-1 text-xs">
+            <p className="truncate">{session?.user?.name}</p>
+            <p className="text-gray-400">1 Karma</p>
+          </div>
+          <ChevronDownIcon className="h-5 flex-shrink-0 text-gray-400"/>
         </div>
-        <p className="text-gray-400">Sign in</p>
-      </div>
+      ) : (
+        <div onClick={() => signIn()} className="hidden cursor-pointer items-center space-x-2 border border-gray-100 p-2 lg:flex">
+          <div className="relative h-5 w-5 flex-shrink-0">
+            <Image src="https://links.papareact.com/23l" layout="fill" objectFit="contain" />
+          </div>
+          <p className="text-gray-400">Sign in</p>
+        </div>
+      )}
     </div>
   )
 }
