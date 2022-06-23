f635aa8dbf9624df2d4269c3dfa0afb89d6d0788
Rigote <matheus.rigote@samuraiexperts.com.br>
2022-06-22 23:39:05 -0300

    [Feature] - Implementando Post Box Layout

---
+++ src/components/PostBox/index.tsx
@@ -0,0 +1,98 @@
+import { LinkIcon, PhotographIcon } from "@heroicons/react/outline"
+import { useSession } from "next-auth/react"
+import Avatar from "../Avatar"
+import {useForm} from 'react-hook-form'
+import { useState } from "react"
+
+type FormData = {
+  postTitle: string
+  postBody: string
+  postImage: string
+  subreddit: string
+}
+
+const PostBox = () => {
+  const {data:session} = useSession()
+  const [imageBoxOpen, setImageBoxOpen] = useState<boolean>(false)
+  const { register, handleSubmit, watch, formState: { errors } } = useForm<FormData>()
+
+  const onSubmit = handleSubmit (async (formData) => {
+    console.log(formData)
+  })
+
+  return (
+    <form onSubmit={onSubmit} className="stick top-16 z-50 rounded-md border border-gray-300 bg-white p-2">
+      <div className="flex items-center space-x-3">
+        <Avatar seed="Gaodin"/>
+        <input
+        {...register('postTitle', {required: true})}
+        disabled={!session}
+        className='flex-1 rounded-md bg-gray-50 p-2 pl-5 outline-none'
+        type='text'
+        placeholder={session ? 'Create a post by entering a title!' : 'Sign in to post'}
+        />
+        <PhotographIcon
+        onClick={() => setImageBoxOpen(!imageBoxOpen)}
+        className={`h-6 text-gray-300 cursor-pointer ${imageBoxOpen && 'text-blue-300'}`}/>
+        <LinkIcon className="h-6 text-gray-300 cursor-pointer"/>
+      </div>
+      {!!watch('postTitle') && (
+        <div className="flex flex-col py-2">
+          {/**Body */}
+          <div className="flex items-center px-2">
+            <p className="min-w-[90px]">Body:</p>
+            <input
+            className="m-2 flex-1 bg-blue-50 p-2 outline-none"
+            {...register('postBody')}
+            type='text'
+            placeholder="Text (Optional)"
+            />
+          </div>
+
+          {/**Subreddit */}
+          <div className="flex items-center px-2">
+            <p className="min-w-[90px]">Subreddit:</p>
+            <input
+            className="m-2 flex-1 bg-blue-50 p-2 outline-none"
+            {...register('subreddit', {required: true})}
+            type='text'
+            placeholder="i.e. reactjs"
+            />
+          </div>
+
+          {/**Imagebox */}
+          {imageBoxOpen && (
+            <div className="flex items-center px-2">
+              <p className="min-w-[90px]">Image URL:</p>
+              <input
+              className="m-2 flex-1 bg-blue-50 p-2 outline-none"
+              {...register("postImage")}
+              type='text'
+              placeholder="Optional..."
+              />
+            </div>
+          )}
+
+          {/**Imagebox */}
+          {Object.keys(errors).length > 0 && (
+            <div className="space-y-2 p-2 text-red-500">
+              {errors.postTitle?.type === 'required' && (
+                <p>- A Post Title is required</p>
+              )}
+
+              {errors.subreddit?.type === 'required' && (
+                <p>- A Subreddit is required</p>
+              )}
+            </div>
+          )}
+
+          {/**Submit button */}
+          {!!watch('postTitle') && <button type="submit" className="w-full rounded-full bg-blue-400 p-2 text-white">Create Post</button>}
+
+        </div>
+      )}
+    </form>
+  )
+}
+
+export default PostBox
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,13 +1,20 @@
 import type { NextPage } from 'next'
 import Head from 'next/head'
+import PostBox from '../components/PostBox'
 
 const Home: NextPage = () => {
   return (
-    <div>
+    <div className='my-7 max-w-5xl mx-auto'>
       <Head>
         <title>Tricolor on The Rock</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
+
+      <PostBox/>
+
+      <div className='flex'>
+        {/**Feed */}
+      </div>
     </div>
   )
 }
